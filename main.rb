fc9fc9b67527ddb1271a9523ec4d20678f2d3fac
James Perrin <jamesperrin88@gmail.com>
2021-04-03 00:03:14 +1000

    initialise

---
+++ main.rb
@@ -0,0 +1,29 @@
+# pre- start checklist
+
+# login- new user, existing user????
+
+# Options - Warnings, see previous list, make new list
+# warnings - show current warnings, edit?
+# Previos list - show last print out
+# make new list - daily, weekly, monthly?
+# daily - fuel, water, tyres, condition - Any warning to add
+# weekly - shocks, belts, oil
+# monthly - grease, oil change, registration, parts?? (after 6 monthly checks add change oil to warnings)
+# update lists
+# p lists
+
+
+
+# require colorize to highlight each option for input
+# warnings = []
+
+
+# puts "Would you like to check: warnings, last print out or new list"
+# menu = gets.chomp
+# # change all input to lowercase
+# if menu == "warnings"
+#   puts warnings
+# elsif menu == "last print out"
+
+# end
+
